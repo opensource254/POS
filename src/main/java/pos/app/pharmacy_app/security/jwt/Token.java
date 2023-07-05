6e3d29ca0b72c030f5fd9d5200a190580293ba72
unknown <Richard Okello>
2023-07-06 00:03:09 +0300

    created entity to hold tokens

---
+++ src/main/java/pos/app/pharmacy_app/security/jwt/Token.java
@@ -0,0 +1,30 @@
+package pos.app.pharmacy_app.security.jwt;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pos.app.pharmacy_app.users.entity.Users;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    public Integer id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public Users user;
+}
