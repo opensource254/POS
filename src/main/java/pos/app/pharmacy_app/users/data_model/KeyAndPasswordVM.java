cb859b2dda897ae13274808569060633b1a5972b
unknown <Richard Okello>
2023-07-12 12:27:28 +0300

    added class to hold key and password details

---
+++ src/main/java/pos/app/pharmacy_app/users/data_model/KeyAndPasswordVM.java
@@ -0,0 +1,22 @@
+package pos.app.pharmacy_app.users.data_model;
+
+public class KeyAndPasswordVM {    private String key;
+
+    private String newPassword;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
