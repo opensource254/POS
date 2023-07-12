c2bfb7fbfa5189f8e6b663aa10ee9a2e163fefb5
unknown <Richard Okello>
2023-07-12 11:34:01 +0300

    added changes to the category response data

--- src/main/java/pos/app/pharmacy_app/categories/Data/CategoryResponse.java
+++ src/main/java/pos/app/pharmacy_app/categories/Data/CategoryResponse.java
@@ -4,13 +4,54 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-public class CategoryResponse {
+public class  CategoryResponse {
     private Long id;
     private String message;
     private String responseCode;
     private String name;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
