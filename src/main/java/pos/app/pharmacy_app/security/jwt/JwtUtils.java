853601cba2a7146b7f8e7d67868e58924b530360
unknown <Richard Okello>
2023-07-12 11:39:36 +0300

    added the utility service class for JWTs

--- src/main/java/pos/app/pharmacy_app/security/jwt/JwtUtils.java
+++ src/main/java/pos/app/pharmacy_app/security/jwt/JwtUtils.java
@@ -1,70 +1,82 @@
 package pos.app.pharmacy_app.security.jwt;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseCookie;
-import org.springframework.stereotype.Component;
-import org.springframework.web.util.WebUtils;
-import pos.app.pharmacy_app.security.services.UserDetailsImplementation;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.security.SignatureException;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+
+import java.security.Key;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
-@Component
+@Service
 public class JwtUtils {
     private Logger logger= LoggerFactory.getLogger(JwtUtils.class);
-    @Value("${pos.app.jwtSecret}")
-    private String jwtSecrete;
-    @Value("${pos.app.jwtExpirationMs}")
-    private String jwtExpirationMs;
-    @Value("${pos.app.jwtCookieName}")
-    private String jwtCookie;
+//    @Value("${pos.app.jwtSecret}")
+//    private String jwtSecreteKey;
+//    @Value("${pos.app.refreshTokenEpiry}")
+//    private Long refreshExpiration;
+//    @Value("${pos.app.jwtExpirationMs}")
+//    private Long jwtExpiration;
+    @Value("${application.security.jwt.secret-key}")
+    private String jwtSecreteKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
 
-    public String getJwtFromCookie(HttpServletRequest request){
-        Cookie cookie= WebUtils.getCookie(request,jwtCookie);
-        if (cookie!=null){
-            return cookie.getValue();
-        }
-            else{
-                return null;
-            }
-        }
-        public ResponseCookie generateJwtCookie(UserDetailsImplementation userPrincipal){
-        String jtw=generateJwtTokenFromUsername(userPrincipal.getUsername());
-        ResponseCookie responseCookie= ResponseCookie.from(jwtCookie,jtw).path("/api")
-                .maxAge(24*60*60).httpOnly(true).build();
-        return responseCookie;
-        }
-        public String getNameFromUsername(String token){
-        return Jwts.parser().setSigningKey(jwtSecrete).parseClaimsJws(token).getBody().getSubject();
-        }
-    public boolean validateJwtToken(String authToken) throws SignatureException {
-        try {
-            Jwts.parser().setSigningKey(jwtSecrete).parseClaimsJws(authToken);
-            return true;
-        } catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
-        } catch (ExpiredJwtException e) {
-            logger.error("JWT token is expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            logger.error("JWT token is unsupported: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            logger.error("JWT claims string is empty: {}", e.getMessage());
-        }
-
-        return false;
+    public String  extractUsernameFromToken(String token){
+        return extractSingleClaim(token,claims -> claims.getSubject());
     }
-    public String generateJwtTokenFromUsername(String username) {
+private Key getSignIngKey(){
+    byte[] keysBytes= Decoders.BASE64.decode(jwtSecreteKey);
+    return Keys.hmacShaKeyFor(keysBytes);
+}
+public Claims extractClaims(String token) {
+      return Jwts.parserBuilder()
+        .setSigningKey(jwtSecreteKey)
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+   }
+    public <T>T extractSingleClaim(String token, Function<Claims,T> claimParser){
+        final Claims claims=extractClaims(token);
+        return claimParser.apply(claims);
+    }
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails, long expiresAt){
         return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecrete)
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expiresAt))
+                .signWith(getSignIngKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+    public String refreshToken(UserDetails userDetails){
+        return  generateToken(new HashMap<>(),userDetails,refreshExpiration);
+    }
+    public String accessToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails, jwtExpiration);
+    }
+    public boolean tokenIsValid(String token, UserDetails userDetails){
+        final  String username= extractUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()))&& isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+        return extractSingleClaim(token,date->date.getExpiration());
     }
     }
 
