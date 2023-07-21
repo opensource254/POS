485aef81567abe59d9ffc2999fb7caccc2ebed9a
unknown <Richard Okello>
2023-07-21 10:29:45 +0300

    added mail deatils class to hold details for sending test emails

--- src/main/java/pos/app/pharmacy_app/users/entity/MailDetails.java
+++ src/main/java/pos/app/pharmacy_app/users/entity/MailDetails.java
@@ -1,2 +1,13 @@
-package pos.app.pharmacy_app.users.entity;public class MailDetails {
+package pos.app.pharmacy_app.users.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MailDetails {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
 }
